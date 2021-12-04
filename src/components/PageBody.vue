e514ddbc37aad806335953eebab707aded9e6f89
schollar <cschollar22@gmail.com>
2021-12-03 18:56:17 -0700

    Changed from if statments to a v-bind on the class to get rid of re-used code

--- src/components/PageBody.vue
+++ src/components/PageBody.vue
@@ -3,14 +3,13 @@
         <!-- Learned about the v-on in a video, same thing as @click and then we change the page layout
         variable to the opposite of what is is -->
         <button v-on:click="page_layout = !page_layout">Toggle View!</button>
-        <!-- If the page_layout variable is true it will render the section with the class row-view -->
-        <section v-if="page_layout === true" class="row-view">
+        <!-- Changed this to a v-bind on the class. If pagelayout is true row-view class is applied
+        if pagelayout is false, column_view is applied as a class. Each class has corresponding CSS to make 
+        this work properly. This way we only have to loop through the tweets once, unlike how i had this before
+        where I was using copy and pasted code-->
+        <section v-bind:class ="{ row_view: page_layout, 'column_view': !page_layout }">
             <!-- Looping through the tweets to show them on the page -->
             <tweet-card v-for="tweet in tweets" :key="tweet.tweet_id" :username="tweet.username" :tweet_content="tweet.tweet_content" :date_created="tweet.date_created"></tweet-card>
-        </section>
-        <!-- If page_layout is false we show the column view and loop through the same way as above -->
-        <section v-else class="column_view">
-        <tweet-card v-for="tweet in tweets" :key="tweet.tweet_id" :username="tweet.username" :tweet_content="tweet.tweet_content" :date_created="tweet.date_created"></tweet-card>
         </section>
     </div>
 </template>
@@ -71,11 +70,13 @@ import TweetCard from './TweetCard.vue'
 <style scoped>
     .row_view {
         display: grid;
-        place-items: center
+        place-items: center;
+        row-gap: 25px
     }
 
         .column_view {
         display: grid;
         grid-auto-flow: column;
+        column-gap: 25px;
     }
 </style>
